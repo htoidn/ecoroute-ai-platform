95f2aa80a0fa911f5e4dd63ed5a8f7a8ace68737
htoidn <htoideen@gmail.com>
2026-05-05 21:50:45 +0200

    Add hello world api with swagger support

---
+++ backend/src/main/java/com/dev/ecoroute/dto/HelloResponse.java
@@ -0,0 +1,13 @@
+package com.dev.ecoroute.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloResponse {
+    private String message;
+}
